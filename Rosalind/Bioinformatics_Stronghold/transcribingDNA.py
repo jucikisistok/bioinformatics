7a1f46bd001bec8c76be0f928ff39d43d081d3ca
Juci Kisistók <judit.kisistok@gmail.com>
2017-07-29 14:00:13 +0200

    new solution added

---
+++ Rosalind/Bioinformatics_Stronghold/transcribingDNA.py
@@ -0,0 +1,21 @@
+import sys
+
+def transcribingDNA(t):
+	"""
+	Input: txt file containing a DNA string t corresponding to a coding strand
+
+	Output: txt file containing the transcribed RNA string u formed by replacing 
+	all occurrences of T in t with U in u
+
+	Usage (via command line): python <filename.py> <input_dataset.txt>
+	"""
+
+	u = t.replace("T", "U")
+	return u
+
+dataset = open(sys.argv[1], "r")
+t = dataset.read()
+with open("result_"+sys.argv[1], "w") as f:
+	print(transcribingDNA(t), file = f)
+	f.close()
+dataset.close()
