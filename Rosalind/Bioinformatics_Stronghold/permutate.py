f7d9770f9cc963e4642b57c0cbd092e88e1dbbe0
Juci Kisistók <judit.kisistok@gmail.com>
2017-08-03 23:35:33 +0200

    new solution added

--- Rosalind/Bioinformatics_Stronghold/permutate.py
+++ Rosalind/Bioinformatics_Stronghold/permutate.py
@@ -13,13 +13,13 @@ def permutate(m):
 	"""
 	This returns the number of permutations of length n and all such permutations.
 	"""
-	
+
 	possible_permutations = list(permutations(range(1, m + 1)))
 	f.write("Number of possible permutations: %s\n"%str(len(possible_permutations)))
 
-	for perm in possible_permutations:
-		string = "".join(str(perm)).replace(",", "")
-		f.write("%s\n"%string)
+	for i in possible_permutations:
+		perm = "".join(str(i)).replace(",", "")
+		f.write("%s\n"%perm)
 
 m = int(input("Choose an integer:"))
 with open("result.txt", "w") as f:
